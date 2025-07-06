1afa73eb484841683dc78d1c525c8f58ce2625fe
Navya Damisetti <navyaratna.deepika14@gmail.com>
2025-07-05 23:50:55 -0400

    started Arrays & Hashing

---
+++ Arrays & Hashing/ContainsDuplicate.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def hasDuplicate_(self, nums: List[int]) -> bool:
+        for i_index, i in enumerate(nums): 
+            for j_index, j in enumerate(nums):      #O(n^2)
+                if(i==j) & (i_index != j_index):
+                    return True
+        return False
+
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        hashset = set()
+        for num in nums:         #O(n)
+            if num in hashset:   #O(1)
+                return True
+            hashset.add(num)     #O(1)
+        return False
+
+    #above solution has Time complexity of O(n)
+
+        
