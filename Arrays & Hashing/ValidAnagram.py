46d9bd55184c376eaca7cc36f7b4f3c5cc5b8147
Navya Damisetti <navyaratna.deepika14@gmail.com>
2025-07-06 23:34:09 -0400

    valid anagram

---
+++ Arrays & Hashing/ValidAnagram.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+        return True
+
+        # time complexity is O(n+m)
+        # space complexity is O(1)
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+        # python's inbuilt datastructure that counts things automatically
+        # it has the same time & space complexity as the above solution
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+        # we can sort & compare, this is brute force solution
+        # but time complexity will be O(nlogn + mlogm)
+        # space complexity will be O(1) or O(n+m)
+
